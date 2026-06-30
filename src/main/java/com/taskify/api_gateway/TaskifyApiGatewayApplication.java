c400ca8b5c372acdd47bf23f442b4caa13a32606
Piyush Kalmegh <p.kalmegh1@gmail.com>
2026-06-30 19:22:31 +0530

    Initial Commit

---
+++ src/main/java/com/taskify/api_gateway/TaskifyApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.taskify.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskifyApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskifyApiGatewayApplication.class, args);
+	}
+
+}
